5ad4d2157db0eeb5c101c54693d867f4cfe631ad
Yurik Rivera <yurikrz@hotmail.com>
2023-09-13 12:43:15 -0600

    test2 en loader  09-13-2023

--- assets/js/cart_main.js
+++ assets/js/cart_main.js
@@ -1,4 +1,13 @@
+import loader from "./components/loader.js";
+import showMenu from './components/showMenu.js'
+
 import {cartMain} from './components/cart.js'
 import { getbd } from './components/products.js'
+
+//Loader
+loader()
+
+//Mostrar Menu
+showMenu()
 
 cartMain(getbd())
--- assets/js/components/loader.js
+++ assets/js/components/loader.js
@@ -1,5 +1,7 @@
 function loader(){
+    console("1")
     window.addEventListener('load',() => {
+        console.log("2")
         const loader = document.querySelector('.loader')
         loader.classList.add('loader--hidden')
         console.log(loader)
