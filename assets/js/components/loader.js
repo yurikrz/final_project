1c47861999635177d9f783c16e1503437a0192b5
Yurik Rivera <yurikrz@hotmail.com>
2023-09-13 12:19:58 -0600

    test en loader  09-13-2023

--- assets/js/components/loader.js
+++ assets/js/components/loader.js
@@ -2,8 +2,8 @@ function loader(){
     window.addEventListener('load',() => {
         const loader = document.querySelector('.loader')
         loader.classList.add('loader--hidden')
+        console.log(loader)
     })
-    console.log(loader)
 }
 
 export default loader
